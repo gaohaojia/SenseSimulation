90eeb917f3fec84be770b98aead67364844499c0
Gao Haojia <2723687563@qq.com>
2024-08-28 21:47:53 +0800

    add ros2 domain id change

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 source install/setup.sh
-export ROS_DOMAIN_ID=$(expr $1 + 1)
-ros2 launch simulation_bringup simulation.launch.py robot_id:=$1
+ros2 launch simulation_bringup simulation.launch.py robot_count:=$1
--- src/simulation_bringup/launch/simulation.launch.py
+++ src/simulation_bringup/launch/simulation.launch.py
@@ -4,43 +4,105 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
+from launch import LaunchDescription, LaunchContext
 from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, TimerAction
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction, OpaqueFunction, SetEnvironmentVariable, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.conditions import LaunchConfigurationEquals
-from launch.conditions import IfCondition
-from launch.actions.append_environment_variable import AppendEnvironmentVariable
 
-# Enum for world types
-class WorldType:
-    room1 = 'room1'
+def robot_description(context: LaunchContext, robot_count, use_sim_time):
+    action_list = []
+    for i in range(int(context.perform_substitution(robot_count))):
+        robot_description = Command(['xacro ', 
+                                    os.path.join(get_package_share_directory('simulation_bringup'), 
+                                                'urdf', 
+                                                'sensebeetle.xacro')])
+        
+        start_joint_state_publisher_cmd = Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            namespace='robot_{}'.format(i),
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_description
+            }],
+            output='screen'
+        )
 
-def get_world_config(world_type):
-    world_configs = {
-        WorldType.room1: {
-            'x': '0.0',
-            'y': '0.0',
-            'z': '0.0',
-            'yaw': '0.0',
-            'world_path': 'room1/room1.world'
-        }
-    }
-    return world_configs.get(world_type, None)
+        start_robot_state_publisher_cmd = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            namespace='robot_{}'.format(i),
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_description
+            }],
+            output='screen'
+        )
+
+        start_spawn_entity_node = Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'robot_{}'.format(i),
+                '-topic', 'robot_{}/robot_description'.format(i),
+                '-robot_namespace', 'robot_{}'.format(i),
+                '-x', str(i * 0.5),
+                '-y', '0.0',
+                '-z', '0.0',
+                '-Y', '0.0'
+            ],
+        )
+
+        integration_service = ExecuteProcess(
+            cmd=['integration-service', os.path.join(get_package_share_directory('simulation_bringup'), 'yaml', 'robot_{}.yaml'.format(i))],
+            output='screen'
+        )
+
+        action_list.append(start_joint_state_publisher_cmd)
+        action_list.append(start_robot_state_publisher_cmd)
+        action_list.append(start_spawn_entity_node)
+        action_list.append(integration_service)
+
+    return action_list
+
+def communication_client_launch(context: LaunchContext, robot_count):
+    action_list = []
+    for i in range(int(context.perform_substitution(robot_count))):
+        communication_client_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('communication_client'), 
+                                                       'launch', 
+                                                       'communication_client.launch.py')),
+            launch_arguments={
+                'robot_id': str(i),
+            }.items()
+        )
+        action_list.append(SetEnvironmentVariable('ROS_DOMAIN_ID', str(i+1)))
+        action_list.append(communication_client_launch)
+    action_list.append(SetEnvironmentVariable('ROS_DOMAIN_ID', str(0)))
+    return action_list
+
+def world_launch(context: LaunchContext, world_name):
+    name = context.perform_substitution(world_name)
+    world_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')),
+        launch_arguments={
+            'world': os.path.join(get_package_share_directory('simulation_bringup'), 'world', name, name + '.world')
+        }.items(),
+    )
+    return [world_launch]
+
 
 def generate_launch_description():
-    # Get the launch directory
-    bringup_dir = get_package_share_directory('simulation_bringup')
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    # Specify xacro path
-    default_robot_description = Command(['xacro ', os.path.join(
-    get_package_share_directory('simulation_bringup'), 'urdf', 'sensebeetle.xacro')])
-
-    # Create the launch configuration variables
+    robot_count = LaunchConfiguration('robot_count')
     use_sim_time = LaunchConfiguration('use_sim_time')
-    robot_description = LaunchConfiguration('robot_description')
+    world_name = LaunchConfiguration('world_name')
+
+    declare_robot_count = DeclareLaunchArgument('robot_count', default_value='3', description='')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -49,88 +111,24 @@ def generate_launch_description():
     )
 
     declare_world_cmd = DeclareLaunchArgument(
-        'world',
-        default_value=WorldType.room1,
+        'world_name',
+        default_value='room1',
         description='Choose world'
     )
 
-    declare_robot_description_cmd = DeclareLaunchArgument(
-        'robot_description',
-        default_value=default_robot_description,
-        description='Robot description'
-    )
-
-    # Specify the actions
     gazebo_client_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')),
     )
 
-    real_to_simulation_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('real_to_simulation'), 'launch', 'real_to_simulation.launch.py'))
-    )
-
-    start_joint_state_publisher_cmd = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_description
-        }],
-        output='screen'
-    )
-
-    start_robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_description
-        }],
-        output='screen'
-    )
-
-    def create_gazebo_launch_group(world_type):
-        world_config = get_world_config(world_type)
-        if world_config is None:
-            return None
-
-        return GroupAction(
-            condition=LaunchConfigurationEquals('world', world_type),
-            actions=[
-                Node(
-                    package='gazebo_ros',
-                    executable='spawn_entity.py',
-                    arguments=[
-                        '-entity', 'robot',
-                        '-topic', 'robot_description',
-                        '-x', world_config['x'],
-                        '-y', world_config['y'],
-                        '-z', world_config['z'],
-                        '-Y', world_config['yaw']
-                    ],
-                ),
-                IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-                    launch_arguments={'world': os.path.join(bringup_dir, 'world', world_config['world_path'])}.items(),
-                )
-            ]
-        )
-
-    bringup_room1_cmd_group = create_gazebo_launch_group(WorldType.room1)
-
-    # Create the launch description and populate
     ld = LaunchDescription()
 
+    ld.add_action(declare_robot_count)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_world_cmd)
-    ld.add_action(declare_robot_description_cmd)
 
     ld.add_action(gazebo_client_launch)
-    ld.add_action(real_to_simulation_launch)
-    ld.add_action(TimerAction(period=2.0, actions=[start_joint_state_publisher_cmd, 
-                                                   start_robot_state_publisher_cmd, 
-                                                   bringup_room1_cmd_group]))
+    ld.add_action(OpaqueFunction(function=world_launch, args=[world_name]))
+    ld.add_action(OpaqueFunction(function=robot_description, args=[robot_count, use_sim_time]))
+    ld.add_action(OpaqueFunction(function=communication_client_launch, args=[robot_count]))
 
     return ld
