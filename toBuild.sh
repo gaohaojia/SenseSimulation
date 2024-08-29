d6fc1fffa11c5f2fc53e6005cf9ea285c625a377
Gao Haojia <2723687563@qq.com>
2024-08-29 21:36:54 +0800

    fix bug

--- toBuild.sh
+++ toBuild.sh
@@ -1,1 +1,1 @@
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DMIX_ROS2_PACKAGES="sensor_msgs geometry_msgs livox_ros_driver2"
